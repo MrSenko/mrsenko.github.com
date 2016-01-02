359c6dadcb0a1e87a0849c38ea8db7800b817cae
Alexander Todorov <atodorov@otb.bg>
2016-01-02 12:17:42 +0200

    add script to upload to s3

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUCKET="senko"
+
+for d in `find ./ -type d`; do
+    dest=`echo $d | sed 's|./||'`
+    s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m application/javascript $d/*.js s3://$BUCKET/$dest/
+    s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m text/css               $d/*.css s3://$BUCKET/$dest/
+done
+
+s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -M ./ s3://$BUCKET/
