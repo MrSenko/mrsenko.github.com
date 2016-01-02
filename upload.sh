85f1797edf8a77f89529d4c0a2e588fa288f2e05
Alexander Todorov <atodorov@otb.bg>
2016-01-02 13:55:24 +0200

    don't upload .git directory to S3

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@
 
 BUCKET="senko"
 
-for d in `find ./ -type d`; do
+for d in `find ./ -type d | grep -v ".git"`; do
     dest=`echo $d | sed 's|./||'`
     s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m application/javascript $d/*.js s3://$BUCKET/$dest/
     s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m text/css               $d/*.css s3://$BUCKET/$dest/
 done
 
-s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -M ./ s3://$BUCKET/
+s3cmd sync -c ../s3.cfg -v -P --no-check-md5 --exclude=".git/*" -M ./ s3://$BUCKET/
