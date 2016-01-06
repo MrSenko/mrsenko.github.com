89656338826d782bb9a80dce456578f8e71daaf4
Alexander Todorov <atodorov@otb.bg>
2016-01-06 16:38:52 +0200

    upload to new S3 bucket

--- upload.sh
+++ upload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-BUCKET="senko"
+BUCKET="mrsenko.com"
+S3CMD=~/private/repos/github/s3cmd/s3cmd
 
 for d in `find ./ -type d | grep -v ".git"`; do
     dest=`echo $d | sed 's|./||'`
-    s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m application/javascript $d/*.js s3://$BUCKET/$dest/
-    s3cmd sync -c ../s3.cfg -v -P --no-check-md5 -m text/css               $d/*.css s3://$BUCKET/$dest/
+    $S3CMD sync -c ../s3.cfg -v -P --no-check-md5 -m application/javascript $d/*.js s3://$BUCKET/$dest/
+    $S3CMD sync -c ../s3.cfg -v -P --no-check-md5 -m text/css               $d/*.css s3://$BUCKET/$dest/
 done
 
-s3cmd sync -c ../s3.cfg -v -P --no-check-md5 --exclude=".git/*" -M ./ s3://$BUCKET/
+$S3CMD sync -c ../s3.cfg -v -P --no-check-md5 --exclude=".git/*" -M ./ s3://$BUCKET/
