a27c2462682404246951f2b6f9b5d7a9375a64ad
Alexander Todorov <atodorov@otb.bg>
2016-01-07 22:31:42 +0200

    render templates before uploading the website

--- upload.sh
+++ upload.sh
@@ -3,6 +3,17 @@
 BUCKET="mrsenko.com"
 S3CMD=~/private/repos/github/s3cmd/s3cmd
 
+# first render HTML from templates
+for tmpl in `find -name "*.jinja"`; do
+    html_file=`echo "$tmpl" | sed "s/.jinja/.html/"`
+    ./render.py "$tmpl" > "$html_file"
+    if [ $? -ne 0 ]; then
+        echo "RENDERING FAILED ...."
+        exit 99
+    fi
+done
+
+
 for d in `find ./ -type d | grep -v ".git"`; do
     dest=`echo $d | sed 's|./||'`
     $S3CMD sync -c ../s3.cfg -v -P --no-check-md5 -m application/javascript $d/*.js s3://$BUCKET/$dest/
